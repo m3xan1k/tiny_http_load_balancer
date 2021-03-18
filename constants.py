7828dd82642b5a5b93d419507bb1c122bb6a9c30
m3xan1k <dev.serzh@gmail.com>
2021-03-18 11:30:05 +0300

    config yaml file, rewrite controllers

--- constants.py
+++
@@ -1,3 +0,0 @@
-class Backends:
-    MANGO = ['localhost:8081', 'localhost:8082']
-    APPLE = ['localhost:9081', 'localhost:9082']
--- loadbalancer.py
+++ loadbalancer.py
@@ -2,34 +2,41 @@ import random
 
 from flask import Flask, request
 import requests
+import yaml
 
-from constants import Backends
 
 loadbalancer = Flask(__name__)
+config_filepath = './config.yaml'
+
+
+def load_config(path):
+    with open(path) as file:
+        config = yaml.load(file, Loader=yaml.FullLoader)
+    return config
+
+
+config = load_config(config_filepath)
 
 
 @loadbalancer.route('/')
 def router():
     host_header = request.headers.get('Host')
-    if host_header == 'www.mango.com':
-        response = requests.get(f'http://{random.choice(Backends.MANGO)}')
-    elif host_header == 'www.apple.com':
-        response = requests.get(f'http://{random.choice(Backends.APPLE)}')
-    else:
-        return 'Not Found', 404
-    return response.content, response.status_code
+    services = config['hosts']
+    for service in services:
+        if host_header == service['host']:
+            response = requests.get(f'http://{random.choice(service["servers"])}')
+            return response.content, response.status_code
+    return 'Not Found', 404
 
 
-@loadbalancer.route('/mango')
-def mango_path():
-    response = requests.get(f'http://{random.choice(Backends.MANGO)}')
-    return response.content, response.status_code
-
-
-@loadbalancer.route('/apple')
-def apple_path():
-    response = requests.get(f'http://{random.choice(Backends.APPLE)}')
-    return response.content, response.status_code
+@loadbalancer.route('/<path>')
+def path_router(path: str):
+    services = config['paths']
+    for service in services:
+        if service['path'] == request.path:
+            response = requests.get(f'http://{random.choice(service["servers"])}')
+            return response.content, response.status_code
+    return 'Not Found', 404
 
 
 if __name__ == '__main__':
